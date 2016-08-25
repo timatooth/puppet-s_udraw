f51b631b9fcd5cfe9a244ab52d13f56e3d820c1e
Tim Sullivan <tsullivan@timatooth.com>
2016-08-26 01:07:12 +1200

    add letsencrypt

--- manifests/init.pp
+++ manifests/init.pp
@@ -16,7 +16,23 @@ class s_udraw {
     content => template('s_udraw/index.html.erb')
   }
 
+  class { ::letsencrypt:
+    email => 'nztims@gmail.com',
+  }
+
+  letsencrypt::certonly { 'capi.udraw.me':
+    plugin        => 'webroot',
+    webroot_paths => ['/var/www/udrawstatic'],
+    manage_cron   => true,
+    cron_success_command => '/bin/systemctl reload nginx.service',
+    require       => [File['/var/www/udrawstatic']],
+  }
+
   nginx::resource::vhost{'capi.udraw.me':
     www_root => '/var/www/udrawstatic',
+    ssl      => true,
+    ssl_cert => '/etc/letsencrypt/live/capi.udraw.me/fullchain.pem',
+    ssl_key  => '/etc/letsencrypt/live/capi.udraw.me/privkey.pem',
+    require  => [letsencrypt::certonly['capi.udraw.me']],
   }
 }
