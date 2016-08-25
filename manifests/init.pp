a4592300e038a2bb8ea0da1129df87ea967f6f38
Tim Sullivan <tsullivan@timatooth.com>
2016-08-25 22:50:45 +1200

    commit all the things

---
+++ manifests/init.pp
@@ -0,0 +1,23 @@
+class s_udraw {
+
+  class{'nginx':
+    manage_repo => true,
+    package_source => 'nginx-mainline'
+  }
+
+  file {'/var/www':
+    ensure => directory,
+  } ->
+  file {'/var/www/udrawstatic':
+    ensure => directory,
+  } ->
+  file {'/var/www/udrawstatic/index.html':
+    ensure  => file,
+    content => template('s_udraw/index.html.erb')
+  }
+
+  nginx::resource::vhost{'capi.udraw.me':
+    www_root => '/var/www/udrawstatic',
+    require  => ['nginx']
+  }
+}
